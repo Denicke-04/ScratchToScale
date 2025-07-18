d1adf8f1d17f84bad24724bd96f03281404febf9
Denicke-04 <denickesolomon2462@gmail.com>
2025-07-18 13:01:25 +0100

    Added Support for aws inference request

--- Cloth_Classification/test_request.py
+++ Cloth_Classification/test_request.py
@@ -1,5 +1,6 @@
 import requests
 url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
+#url = 'https://nuupb201g1.execute-api.eu-west-1.amazonaws.com/cloth-classification-test/classify' ##aws url
 data = {'url':'https://bit.ly/hat-test'}
 results = requests.post(url,json = data)
 print(results.text)
